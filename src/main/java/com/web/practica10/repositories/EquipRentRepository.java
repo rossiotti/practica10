552256ae2374961b65ea497150f2843d99058fe7
ronaldmariotti <dbzronald_3@hotmail.com>
2018-11-05 16:52:05 -0400

    fix a dos repositorios.

--- src/main/java/com/web/practica10/repositories/EquipRentRepository.java
+++ src/main/java/com/web/practica10/repositories/EquipRentRepository.java
@@ -3,10 +3,11 @@ package com.web.practica10.repositories;
 import com.web.practica10.entity.Equip;
 import com.web.practica10.entity.EquipRental;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.Entity;
 
-@Entity
+@Repository
 public interface EquipRentRepository extends JpaRepository<EquipRental, Integer> {
 
     EquipRental findById(int id);
--- src/main/java/com/web/practica10/repositories/EquipRepository.java
+++ src/main/java/com/web/practica10/repositories/EquipRepository.java
@@ -2,11 +2,12 @@ package com.web.practica10.repositories;
 
 import com.web.practica10.entity.Equip;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.Entity;
 import java.util.List;
 
-@Entity
+@Repository
 public interface EquipRepository  extends JpaRepository<Equip, Integer> {
 
     Equip findById(int id);
